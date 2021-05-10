c275325d9de9dc5407f77b4687739526fdb30f0c
Gage <gagewaguespack@gmail.com>
2021-05-10 14:47:49 -0500

    Started BulletSpawnBehaviour and added new input manager package

---
+++ Assets/Scripts/Gameplay/BulletSpawnBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawnBehaviour : MonoBehaviour
+{
+    [Tooltip("The bullet that will be shot.")]
+    [SerializeField]
+    private GameObject _bullet;
+    [Tooltip("The time it takes before the bullet despawns.")]
+    [SerializeField]
+    private float _despawnTime;
+
+    //Shoots a bullet when called
+    public void Shoot()
+    {
+        //Spawn a bullet
+        Instantiate(_bullet, transform.position, transform.rotation);
+    }
+
+    private void Update()
+    {
+        Destroy(_bullet, _despawnTime);
+    }
+}
