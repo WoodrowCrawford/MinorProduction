3453762fff433e4fad33f4d009172dffa0eeb9ca
Gage <gagewaguespack@gmail.com>
2021-05-17 12:29:05 -0500

    Implemented new movement for enemies and updated prefabs!

--- Assets/Scripts/Movement/EnemyMovementBehaviour.cs
+++ Assets/Scripts/Movement/EnemyMovementBehaviour.cs
@@ -10,12 +10,6 @@ public class EnemyMovementBehaviour : MonoBehaviour
     [Tooltip("The rigidbody attached to this object")]
     [SerializeField]
     private Rigidbody _rigidbody;
-    [Tooltip("The primary target the enemy will be seeking towards.")]
-    [SerializeField]
-    private Transform _target;
-    [Tooltip("The target the enemy will hit before the primary target.")]
-    [SerializeField]
-    private Transform _tempTarget;
     [Tooltip("The NavMeshAgent.")]
     private NavMeshAgent _navMeshAgent;
     [Tooltip("The force that will be applied to object to move it.")]
@@ -24,52 +18,42 @@ public class EnemyMovementBehaviour : MonoBehaviour
     [Tooltip("The maximum magnitude this enemy's velocity can have.")]
     [SerializeField]
     private float _maxVelocity;
+    [Tooltip("The transform position of the enemy.")]
+    [SerializeField]
+    private Transform _currentPosition;
+    [Tooltip("The minimum X position for the enemy.")]
+    [SerializeField]
+    private Transform _minX;
+    [Tooltip("The maximum X position for the enemy.")]
+    [SerializeField]
+    private Transform _maxX;
+    [Tooltip("The max z that the enemy can go.")]
+    [SerializeField]
+    private Transform _zMin;
 
-    //The target is set here.
-    public Transform Target
-    {
-        get
-        {
-            return _target;
-        }
-        set
-        {
-            _target = value;
-        }
-    }
-
-    public Transform TempTarget
-    {
-        get
-        {
-            return _tempTarget;
-        }
-        set
-        {
-            _tempTarget = value;
-        }
-    }
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         //Get a reference to the attached rigidbody
         _rigidbody = GetComponent<Rigidbody>();
         _navMeshAgent = GetComponent<NavMeshAgent>();
+
+        //sets the destination to be the max X position
+        Vector3 tempPos = new Vector3(_maxX.transform.position.x, _currentPosition.transform.position.y, _currentPosition.transform.position.z);
+        _navMeshAgent.SetDestination(tempPos);
     }
 
-    private void Update()
+    void Update()
     {
-        //If the temp target has been hit
-        if (!TempTarget)
+        if (_currentPosition.transform.position.x >= _maxX.transform.position.x)
         {
-            //Sets the targets position to be the enemies destination.
-            _navMeshAgent.SetDestination(_target.position);
+            Vector3 leftPos = new Vector3(_minX.transform.position.x, _currentPosition.transform.position.y, _currentPosition.transform.position.z - 2);
+            _navMeshAgent.SetDestination(leftPos);
         }
-        else
+
+        if (_currentPosition.transform.position.x <= _minX.transform.position.x)
         {
-            //Sets the targets position to be a temporary destination.
-            _navMeshAgent.SetDestination(_tempTarget.position);
+            Vector3 rightPos = new Vector3(_maxX.transform.position.x, _currentPosition.transform.position.y, _currentPosition.transform.position.z - 2);
+            _navMeshAgent.SetDestination(rightPos);
         }
     }
 }
