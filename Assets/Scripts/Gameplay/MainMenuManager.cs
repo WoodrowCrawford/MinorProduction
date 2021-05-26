1c026630f1cf3c52c8834b3b661878420fd3e262
WoodrowCrawford <woodrowcrawford03@gmail.com>
2021-05-26 15:53:11 -0500

    Added Main menu; Still buggy but works
    Made a main menu that has a play and quit button. Right now the HUD from the game still shows in the menu but other than that everything works as intended!

---
+++ Assets/Scripts/Gameplay/MainMenuManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuManager : MonoBehaviour
+{
+    //The main menu screen
+    [SerializeField]
+    private GameObject _mainMenuScreen;
+
+
+    //What happens when the player clicks start on the main menu
+    public void StartGame()
+    {
+        SceneManager.LoadScene("UITestingScene");
+    }
+
+    //What happens when the game quits
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
